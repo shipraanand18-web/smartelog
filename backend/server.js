c66a6cf70de9d497ef5e5c7522a5d1e1a531c82b
Shipra Anand <shipra.anand18@gmail.com>
2026-04-22 18:29:22 +0530

    use cwd for static

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,8 @@ const io = new Server(server, {
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "../frontend")));
+//app.use(express.static(path.join(__dirname, "../frontend")));
+app.use(express.static(path.join(process.cwd(), "frontend")));
 
 // =============================================
 // IN-MEMORY STORE
