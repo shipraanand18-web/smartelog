bad64766fc5eed8cf5e21ff60a026c4d0665044c
Shipra Anand <shipra.anand18@gmail.com>
2026-04-22 17:56:30 +0530

    railway fix

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const io = new Server(server, {
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "../frontend")));
+app.use(express.static(path.join(__dirname, "/frontend")));
 
 // =============================================
 // IN-MEMORY STORE
